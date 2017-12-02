feed74de23a4c690960f0168ba1232f5bce78a5c
dhouibd <donia.dhouib@phelma.grenoble-inp.fr>
2017-12-02 11:19:29 +0100

    Partie 3.3.2

--- ensimag-video/src/ensivideo.c
+++ ensimag-video/src/ensivideo.c
@@ -21,19 +21,29 @@ int main(int argc, char *argv[]) {
     res = SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO|SDL_INIT_EVENTS);
     atexit(SDL_Quit);
     assert(res == 0);
-    
+
+
     // start the two stream readers
+    pthread_t ReadImg, ReadSon ;
 
-    
+    pthread_create(&ReadImg,NULL,theoraStreamReader,argv[1]);
+    pthread_create(&ReadSon,NULL,vorbisStreamReader,argv[1]);
+
     // wait audio thread
-
+    pthread_cond_t c;
+    pthread_cond_init(&c,NULL);
+    pthread_cond_wait(&c,&ReadSon);
     // 1 seconde de garde pour le son,
     sleep(1);
 
     // tuer les deux threads videos si ils sont bloqués
+    pthread_cancel(&ReadSon);
+    pthread_cancel(&ReadImg);
 
     // attendre les 2 threads videos
+    pthread_cond_wait(&c,&ReadSon);
+    pthread_cond_wait(&c,&ReadImg);
 
-    
-    exit(EXIT_SUCCESS);    
+    exit(EXIT_SUCCESS);
+
 }
--- ensimag-video/src/synchro.c
+++ ensimag-video/src/synchro.c
@@ -1,15 +1,29 @@
 #include "synchro.h"
 #include "ensitheora.h"
 
-
+int vides = 30 ;
 bool fini;
+cond fp ; //con d'attente pour les producteurs
+cond fc ; //con d'attente pour les consommateurs
 
 /* les variables pour la synchro, ici */
 
+pthread_mutex_t m;
+pthread_mutex_init(&m, NULL);
+pthread_cond_t vide, plein;
+//pthread_cond_init(&vide,NULL);
+//pthread_cond_init(&plein,NULL);
+int Taille, TailleFenetre;
+Taille = 0;
+pthread_cond_t fConsom, fDepot;
+pthread_cond_init(&fConsom,NULL);
+pthread_cond_init(&fDepot,NULL);
 
 
 /* l'implantation des fonctions de synchro ici */
 void envoiTailleFenetre(th_ycbcr_buffer buffer) {
+  windowsx := buffer[0].width;
+  windowsy := buffer[0].length;
 }
 
 void attendreTailleFenetre() {
@@ -22,14 +36,46 @@ void attendreFenetreTexture() {
 }
 
 void debutConsommerTexture() {
+  pthread_mutex_lock(&m);
+  while(vides==NBTEX){
+    pthread_cond_wait(&fConsom,&m);
+  }
+  vides --;
+  //consommation
+  //pthread_cond_broadcast(&plein);
+  pthread_mutex_unlock(&m);
 }
 
 void finConsommerTexture() {
+  pthread_mutex_lock(&m);
+    pthread_cond_signal(&fDepot);
+  pthread_mutex_unlock(&m);
 }
 
 
 void debutDeposerTexture() {
+  pthread_mutex_lock(&m);
+  if(vides == 0){
+    pthread_cond_wait(&fDepot,&m);
+  }
+  vides ++;
+  pthread_mutex_unlock(&m);
 }
 
 void finDeposerTexture() {
+  pthread_mutex_lock(&m);
+    pthread_cond_signal(&fConsom);
+  pthread_mutex_unlock(&m);
+}
+
+    pthread_cond_wait(&fDepot,&m);
+  }
+  vides ++;
+  pthread_mutex_unlock(&m);
+}
+
+void finDeposerTexture() {
+  pthread_mutex_lock(&m);
+    pthread_cond_signal(&fConsom);
+  pthread_mutex_unlock(&m);
 }
